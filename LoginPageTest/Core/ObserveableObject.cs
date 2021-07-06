468bfb7805885cb20c6b9344fe08feb7a1f7c6d0
Mahan <m.mahmodi1380@gmail.com>
2021-07-07 01:54:21 +0430

    Login,register and Payment pages frontend done.

---
+++ LoginPageTest/Core/ObserveableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPageTest.Core
+{
+    class ObserveableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string Name = null )
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+        }
+    }
+}
---
+++ LoginPageTest/Core/RelayCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LoginPageTest.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> Execute;
+        private Func<object, bool> CanExecute;
+    }
+}
